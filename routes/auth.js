e54111fc9f2d84e1c6322ef2231ee63b1d9150a0
Vijay <kvijay66071@gmail.com>
2024-09-15 23:37:06 +0530

    first commit

---
+++ routes/auth.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const passport = require('passport')
+const router = express.Router()
+
+// @desc    Auth with Google
+// @route   GET /auth/google
+router.get('/google', passport.authenticate('google', { scope: ['profile'] }))
+
+// @desc    Google auth callback
+// @route   GET /auth/google/callback
+router.get(
+  '/google/callback',
+  passport.authenticate('google', { failureRedirect: '/' }),
+  (req, res) => {
+    res.redirect('/dashboard')
+  }
+)
+
+// @desc    Logout user
+// @route   /auth/logout
+router.get('/logout', (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      // Handle the error
+      return next(err);
+    }
+    // Redirect after successful logout
+    res.redirect('/');
+  });
+});
+  
+module.exports = router
